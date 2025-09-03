64384f3efd706e4ba8d641974bbedf22714fb989
Aditya-Debnath <109604237+Aditya-Debnath@users.noreply.github.com>
2025-09-04 00:49:22 +0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -74,7 +74,7 @@ if(!$user_id) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Shopping Cart | BookExpress</title>
+  <title>Shopping Cart | Boi Lagbe!</title>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="style.css">
